3ffe07bd6d858c723fe2743403db235431d4ff9b
dse-usa <78096271+dse-usa@users.noreply.github.com>
2021-03-10 14:41:59 -0500

    Refactored script.js getRandomQuote
    Eliminated arrayLength variable used as a multiplier of a random and used array.lenght instead

--- js/script.js
+++ js/script.js
@@ -10,34 +10,32 @@ let quotes = [
   {quote:'I have a dream...', source:'Martin Luther King, Jr.', citation:'March on Washington speech', year:'1963'},
   {quote:"Th-th-th-that's all folks!", source:'Porky the Pig', citation:'Loony Tunes', year:'1935'},
   {quote:'To infinity and beyong', source:'Buzz Lightyear', citation:'Toy Store',year:'1995'},
-  {quote:"test.", source:'none',citation:'',year:''}, 
   {quote:"It’s like déjà vu all over again.", source:'Yogi Berra',citation:'',year:''} 
   
-
   ]
-let numberOfQuotes = quotes.length; 
+
 
 function getRandomQuote (array){
   
-    let randomQuoteNumber =  Math.floor(Math.random() * numberOfQuotes);
+    let randomQuoteNumber =  Math.floor(Math.random() * array.length);
     
     return quotes [randomQuoteNumber]
   }
 
 function printQuote (){
     let randomQuote = getRandomQuote(quotes);
+  
       
     let html =`<p class="quote">${randomQuote.quote}</p>
           <p class ="source">${randomQuote.source}<span`;
-    
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment
       if (randomQuote.citation.length > 0) {
             html += `<span class = "citation">${randomQuote.citation}</span>`;
-            console.log(html);
-        }
+          }
     
       if (randomQuote.year.length > 0) {
           html += `<span class = "year">${randomQuote.year}</span>`;
-        }
+          }
       document.getElementById("quote-box").innerHTML = `${html}`;
        
       }
