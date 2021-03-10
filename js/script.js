5cbdf9de0bc74904ac264ba01bebf9196718b8f7
dse-usa <78096271+dse-usa@users.noreply.github.com>
2021-03-10 13:28:22 -0500

    Updated script.js - Working Version complete
    Completed the printQuote function and tested if statements for skipping null values. Cleaned up  commented out directions and code.

--- js/script.js
+++ js/script.js
@@ -3,55 +3,44 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
 let quotes = [
 
-  {quote:'Ask not what your country can do for you—ask what you can do for your country', source:'John F Kennedy', citation:'Inaugural address', year:'1961'},
-  {quote:'I have a dream', source:'Martin Luther King, Jr.', citation:'March on Washington speech', year:'1963'},
+  {quote:'Ask not what your country can do for you—ask what you can do for your country.', source:'John F Kennedy', citation:'Inaugural address', year:'1961'},
+  {quote:'I have a dream...', source:'Martin Luther King, Jr.', citation:'March on Washington speech', year:'1963'},
   {quote:"Th-th-th-that's all folks!", source:'Porky the Pig', citation:'Loony Tunes', year:'1935'},
   {quote:'To infinity and beyong', source:'Buzz Lightyear', citation:'Toy Store',year:'1995'},
-  {quote:"test.", source:'none',citation:'',year:''} 
+  {quote:"test.", source:'none',citation:'',year:''}, 
+  {quote:"It’s like déjà vu all over again.", source:'Yogi Berra',citation:'',year:''} 
+  
 
-]
-
-
-/***
- * `getRandomQuote` function
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
- * https://w.trhou.se/j8jfv1x8ra 2_loops.js
-***/
+  ]
 let numberOfQuotes = quotes.length; 
 
-function getRandomQuote (quotes){
+function getRandomQuote (array){
   
     let randomQuoteNumber =  Math.floor(Math.random() * numberOfQuotes);
     
     return quotes [randomQuoteNumber]
-}
+  }
 
-
-/***
- * `printQuote` function
- * https://stackoverflow.com/questions/28418119/passing-a-javascript-string-to-a-div-id-div1-tag
-***/
-//console.log (getRandomQuote(quotes));
-/**<p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-        <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p> */
-       let html = `<p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-       <p class="source">P. McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>`;
-       document.getElementById("quote-box").innerHTML = `${html}`;
-       //let test = document.getElementById(load-quote);
-       console.log ('test');
-
-/***s
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+function printQuote (){
+    let randomQuote = getRandomQuote(quotes);
+      
+    let html =`<p class="quote">${randomQuote.quote}</p>
+          <p class ="source">${randomQuote.source}<span`;
+    
+      if (randomQuote.citation.length > 0) {
+            html += `<span class = "citation">${randomQuote.citation}</span>`;
+            console.log(html);
+        }
+    
+      if (randomQuote.year.length > 0) {
+          html += `<span class = "year">${randomQuote.year}</span>`;
+        }
+      document.getElementById("quote-box").innerHTML = `${html}`;
+       
+      }
+       
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
